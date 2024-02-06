01335190e0fb00a7faad65eae64c3b65134bb5e5
emhege <emhege@floridauniversitaria.es>
2024-02-06 11:45:02 +0100

    cd funciona

--- tests/enanaTest.php
+++ tests/enanaTest.php
@@ -14,6 +14,7 @@ class EnanaTest extends TestCase {
         #Se probará el efecto de una herida leve a una Enana con puntos de vida suficientes para sobrevivir al ataque
         #Se tendrá que probar que la vida es mayor que 0 y además que su situación es viva
 
+        
     }
 
     public function testHeridaLeveMuere() {
